df33ab1dc0808a17c84335f8d2c0772779cffbc0
Jacob <jacob21cortez@gmail.com>
2024-04-05 16:59:37 -0400

    map is done

---
+++ graphics/Pacman_map.tsx
@@ -0,0 +1,12 @@
+
+
+<? xml version = "1.0" encoding = "UTF-8" ?>
+<tileset version="1.10" tiledversion="1.10.2" name="Pacman_map" tilewidth="482" tileheight="436" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="482" height="436" source="black.png"/>
+ </tile>
+ <tile id="1">
+  <image width="482" height="436" source="blue.png"/>
+ </tile>
+</tileset>
